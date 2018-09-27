703133ee4b70e759c1189994ef10cd6238aca01c
snawasth <sneha.awasthi@capgemini.com>
2018-09-27 19:52:03 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import './App.css';
-import Loginscreen from './Loginscreen'
+import Loginscreen from './Loginscreen';
+import UploadScreen from './UploadScreen'
+
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
@@ -14,20 +16,20 @@ class App extends Component {
       uploadScreen:[]
     }
   }
-  // componentWillMount(){
-  //   var loginPage =[];
-  //   loginPage.push(<Loginscreen parentContext={this}/>);
-  //   this.setState({
-  //                 loginPage:loginPage
-  //                   })
+  componentWillMount(){
+    var loginPage =[];
+    loginPage.push(<Loginscreen parentContext={this}/>);
+    this.setState({
+                  loginPage:loginPage
+                    })
 
-  //                   if(response.data.code == 200){
-  //                     console.log("Login successfull");
-  //                     var uploadScreen=[];
-  //                     uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
-  //                     self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
-  //                     }
-  // }
+                    // if(response.data.code == 200){
+                    //   console.log("Login successfull");
+                    //   var uploadScreen=[];
+                    //   uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
+                    //   self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
+                    //   }
+  }
   render() {
     return (
       <div className="App">
--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
 import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
@@ -56,35 +55,35 @@ render() {
     
   }
 
-  // handleClick(event){
-  //   var apiBaseUrl = "http://localhost:4000/api/";
-  //   var self = this;
-  //   var payload={
-  //   "email":this.state.username,
-  //   "password":this.state.password
-  //   }
-  //   axios.post(apiBaseUrl+'login', payload)
-  //   .then(function (response) {
-  //   console.log(response);
-  //   if(response.data.code == 200){
-  //   console.log("Login successfull");
-  //   var uploadScreen=[];
-  //   uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
-  //   self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
-  //   }
-  //   else if(response.data.code == 204){
-  //   console.log("Username password do not match");
-  //   alert("username password do not match")
-  //   }
-  //   else{
-  //   console.log("Username does not exists");
-  //   alert("Username does not exist");
-  //   }
-  //   })
-  //   .catch(function (error) {
-  //   console.log(error);
-  //   });
-  //   }
+  handleClick(event){
+    var apiBaseUrl = "http://localhost:4000/api/";
+    var self = this;
+    var payload={
+    "email":this.state.username,
+    "password":this.state.password
+    }
+    axios.post(apiBaseUrl+'login', payload)
+    .then(function (response) {
+    console.log(response);
+    if(response.data.code == 200){
+    console.log("Login successfull");
+    var uploadScreen=[];
+    uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
+    self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
+    }
+    else if(response.data.code == 204){
+    console.log("Username password do not match");
+    alert("username password do not match")
+    }
+    else{
+    console.log("Username does not exists");
+    alert("Username does not exist");
+    }
+    })
+    .catch(function (error) {
+    console.log(error);
+    });
+    }
 }
 
 
