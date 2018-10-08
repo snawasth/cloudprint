e590293dfa5fabee0be731c02a625fd3a558bde2
snawasth <sneha.awasthi@capgemini.com>
2018-10-08 19:40:26 +0530

    Hi Pinguuu

--- gitbatch.bat
+++ gitbatch.bat
@@ -1,8 +1,8 @@
 @echo off
 call git add .
 set /p id="Enter Commit Description: "\r\n
-echo Wait for Process to complete.....
 call git commit -m "%id%"
+echo Wait for Process to complete.....
 REM call git push https://github.com/snawasth/cloudprint.git
 call git push origin master
 pause
